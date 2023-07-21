39bc9b57f691083f27f01391855ffbf275e096a2
iamLalitha <lalithasans10@gmail.com>
2023-07-21 17:03:22 +0530

    server1.3

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express= require('express');
 const app=express();
+
+//middleware
+app.use(express.json());
 let notes=[
     {
         id:1,
@@ -25,9 +28,26 @@ app.get('/',(request,response)=>{
 });
 //to get all the notes-for that we are setting an another endpoint
 app.get('/api/notes',(request,response)=>{
-    response.json(notes);
+    response.status(200).json(notes);
 })
+//create a new resource based on the request data
+app.post('/api/notes',(request,response)=>{
+    // console.log(request.body);
+   notes= notes.concat(request.body);
+    response.status(201).json({message:'note created successfully'})
+});
 
+//fetches a single resource based on the request data 
+app.post('/api/notes/:id',(request,response)=>{
+   const id=request.params.id;
+   const note=notes.find(note=>note.id==id);
+  if(note){
+    response.status(200).json(note);
+  }
+  else{
+    response.status(404).end('id does not exists');
+  }
+})
 
 //we need to define a port to listen for request
 const PORT =3001;
